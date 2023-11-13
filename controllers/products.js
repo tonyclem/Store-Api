7620961410166a22c91e7e53cdf8284e9597a84f
tonyclem <tonyclem001@gmail.com>
2023-11-14 00:24:23 +0100

    second commit, working on controllers

--- controllers/products.js
+++ controllers/products.js
@@ -1,10 +1,28 @@
+const Product = require('../models/products')
+
 const getAllProductsStatic = async (req, res) => {
   // throw new Error('Cannot get all products')
-  res.status(200).json({ msg: 'Product testing routes' })
+  const products = await Product.find({ name: 'vase table' })
+  res.status(200).json({ products, ubHits: products.length })
 }
 
 const getAllProducts = async (req, res) => {
-  res.status(200).json({ msg: 'product routes' })
-}
+  const { featured, company, name } = req.query
+  const queryObject = {}
+  if (featured) {
+    queryObject.featured = featured === 'true' ? true : false
+  }
 
+  if (company) {
+    queryObject.company = company
+  }
+
+  if (name) {
+    queryObject.name = name
+  }
+
+  console.log(queryObject)
+  const products = await Product.find(queryObject)
+  res.status(200).json({ products, ubHits: products.length })
+}
 module.exports = { getAllProducts, getAllProductsStatic }
