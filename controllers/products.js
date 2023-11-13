3704c90f418bade55b2250bd0ade17904d4f705d
tonyclem <tonyclem001@gmail.com>
2023-11-13 23:25:08 +0100

    first commit

--- controllers/products.js
+++ controllers/products.js
@@ -1,9 +1,10 @@
 const getAllProductsStatic = async (req, res) => {
+  // throw new Error('Cannot get all products')
   res.status(200).json({ msg: 'Product testing routes' })
 }
 
 const getAllProducts = async (req, res) => {
-  res.status(200).json({ msg: 'product routes ' })
+  res.status(200).json({ msg: 'product routes' })
 }
 
 module.exports = { getAllProducts, getAllProductsStatic }
--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,4 +1,5 @@
 const errorHandlerMiddleware = (err, req, res, next) => {
+  // console.log(err)
   return res
     .status(500)
     .json({ msg: 'something went wrong, please try again later' })
