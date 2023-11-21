73896eddaa0ada760c662f92502414cfd3b51033
tonyclem <tonyclem001@gmail.com>
2023-11-22 00:39:44 +0100

    sort method to controllers

--- controllers/products.js
+++ controllers/products.js
@@ -2,16 +2,13 @@ const Product = require('../models/products')
 
 const getAllProductsStatic = async (req, res) => {
   // throw new Error('Cannot get all products')
-  const search = 'a'
 
-  const products = await Product.find({
-    name: { $regex: search, $options: 'i' },
-  })
+  const products = await Product.find({}).sort('-name price')
   res.status(200).json({ products, ubHits: products.length })
 }
 
 const getAllProducts = async (req, res) => {
-  const { featured, company, name } = req.query
+  const { featured, company, name, sort } = req.query
   const queryObject = {}
   if (featured) {
     queryObject.featured = featured === 'true' ? true : false
@@ -24,9 +21,17 @@ const getAllProducts = async (req, res) => {
   if (name) {
     queryObject.name = { $regex: name, $options: 'i' }
   }
-
   console.log(queryObject)
-  const products = await Product.find(queryObject)
+
+  let result = Product.find(queryObject)
+  if (sort) {
+    // products = products.sort()
+    const sortList = sort.split(',').join(' ')
+    result = result.sort(sortList)
+  } else {
+    result = result.sort('createdAt')
+  }
+  const products = await result
   res.status(200).json({ products, ubHits: products.length })
 }
 module.exports = { getAllProducts, getAllProductsStatic }
